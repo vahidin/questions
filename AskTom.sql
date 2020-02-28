d3059c5ead1838ed27b2953be318541986fa16cd
Александр Кононов <aykononov@yandex.ru>
2020-02-28 18:23:54 +0300

    Delete AskTom.sql

--- AskTom.sql
+++
@@ -1,9 +0,0 @@
-SELECT v.segment_name, 
-       v.segment_type, 
-       v.bytes/1024 AS kb,
-       v.blocks,
-       v.extents,
-       v.initial_extent
-  FROM User_Segments v;
-
-SELECT * FROM v$undostat;
