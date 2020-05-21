541b9ce977dda000543289b2c53d19fc0dceba59
Александр Кононов <aykononov@yandex.ru>
2020-05-21 13:49:42 +0300

    Update _GROUPING_ROLLUP.sql

--- _GROUPING_ROLLUP.sql
+++ _GROUPING_ROLLUP.sql
@@ -4,8 +4,8 @@ SELECT DECODE(GROUPING(deptno), 0, to_char(deptno), 'Total') AS deptno,
  GROUP BY ROLLUP(deptno);
 
 -------------------------
-DEPTNO	CNT
-10	      3
-20	      5
-30	      6
-Total	   14
+DEPTNO	 CNT
+10	   3
+20	   5
+30	   6
+Total	  14
