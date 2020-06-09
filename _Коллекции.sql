2db6d5d83478be084134507d18ded55457b9cd48
Александр Кононов <aykononov@yandex.ru>
2020-06-09 13:39:00 +0300

    Update _Коллекции.sql

--- _Коллекции.sql
+++ _Коллекции.sql
@@ -1,19 +1,16 @@
 /* Ассоциативный массив */
 DECLARE
-    -- Объявление ассоциативного массива.
-		TYPE ENAME_TYPE IS TABLE OF SCOTT.EMP.ename%TYPE
-    INDEX BY PLS_INTEGER;
-    TYPE LOC_TYPE   IS TABLE OF SCOTT.DEPT.loc%TYPE
-    INDEX BY PLS_INTEGER;
-    -- Объявление колллекции на базе типа ENAME_TYPE.
-		t_ename ENAME_TYPE;
-    t_loc   LOC_TYPE;
-    
-		l_row   PLS_INTEGER;
-  
+  -- Объявление ассоциативного массива.
+  TYPE ENAME_TYPE IS TABLE OF SCOTT.EMP.ename%TYPE INDEX BY PLS_INTEGER;
+  TYPE LOC_TYPE IS TABLE OF SCOTT.DEPT.loc%TYPE INDEX BY PLS_INTEGER;
+  -- Объявление колллекции на базе типа ENAME_TYPE.
+  t_ename ENAME_TYPE;
+  t_loc   LOC_TYPE;
+
+  l_row PLS_INTEGER;
+
   -- Объявление курсора
-	CURSOR ename_loc_cur
-  IS
+  CURSOR ename_loc_cur IS
     SELECT e.ename,
            d.loc
       FROM SCOTT.EMP  e,
@@ -21,26 +18,28 @@ DECLARE
      WHERE e.deptno = d.deptno;
 
 BEGIN
-     -- Заполнение колллекции
-     FOR i IN ename_loc_cur
-     LOOP
-         t_ename(ename_loc_cur%ROWCOUNT) := i.ename;
-         
-         t_loc(ename_loc_cur%ROWCOUNT) := i.loc;
-     END LOOP;
-     
-		 -- Первый индекс колллекции (1)
-     l_row := t_ename.FIRST;
+  -- Заполнение колллекции
+  FOR i IN ename_loc_cur
+  LOOP
+    t_ename(ename_loc_cur%ROWCOUNT) := i.ename;
+  
+    t_loc(ename_loc_cur%ROWCOUNT) := i.loc;
+  END LOOP;
 
-     dbms_output.put_line(rpad('   ENAME',10,' ')||'LOCATION');
-     dbms_output.put_line('-------------------');
-     
-		 -- Вывод коллекции
-     WHILE (l_row IS NOT NULL)
-     LOOP
-       dbms_output.put_line(lpad(l_row,2,' ')||' ' || rpad(t_ename(l_row),7,' ') || t_loc(l_row));
-       l_row := t_ename.NEXT(l_row);
-     END LOOP;   
+  -- Первый индекс колллекции (1)
+  l_row := t_ename.FIRST;
+
+  dbms_output.put_line(rpad('   ENAME', 10, ' ') || 'LOCATION');
+  dbms_output.put_line('-------------------');
+
+  -- Вывод коллекции
+  WHILE (l_row IS NOT NULL)
+  LOOP
+    dbms_output.put_line(lpad(l_row, 2, ' ')   || ' ' ||
+                         rpad(t_ename(l_row), 7, ' ') || 
+			 t_loc(l_row));
+    l_row := t_ename.NEXT(l_row);
+  END LOOP;
 END;
 
    ENAME  LOCATION
