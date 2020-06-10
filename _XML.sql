167a4bd8787d2885aef7e9d59bd8606905b93c7d
Александр Кононов <aykononov@yandex.ru>
2020-06-10 18:14:05 +0300

    Update _XML.sql

--- _XML.sql
+++ _XML.sql
@@ -24,3 +24,26 @@ SELECT  t.xml.extract('//word')                AS c1
 ----------------------------  --------------------------  -----
   <word seq="1">Hello</word>  <word seq="1">Hello</word>  world
   <word seq="2">world</word>
+
+
+SELECT d.extract('//ENTITY//ATTRIBUTE[@name="SURNAME"]/@value').getStringVal() surname
+  FROM TABLE(XMLSequence(XMLType('<?xml version="1.0" encoding="windows-1257" ?>
+<RESPONSE>
+  <DATA REQUEST_ID="111">
+    <ENTITY name="PACKAGE_001">
+      <ATTRIBUTE name="PERS_ID" value="1111" />
+      <ATTRIBUTE name="FIRST_NAME" value="OLGA" />
+      <ATTRIBUTE name="SURNAME" value="NOVIKOVA" />
+    </ENTITY>
+    <ENTITY name="PACKAGE_001">
+      <ATTRIBUTE name="PERS_ID" value="2222" />
+      <ATTRIBUTE name="FIRST_NAME" value="ANNA" />
+      <ATTRIBUTE name="SURNAME" value="ANTONOVA" />
+    </ENTITY>
+  </DATA>
+</RESPONSE>').extract('RESPONSE/DATA/ENTITY'))) d;
+
+   SURNAME
+-----------
+1	 NOVIKOVA
+2	 ANTONOVA
