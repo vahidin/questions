dd2f18c60b85f88ddc366d1622a6505a1adde405
Александр Кононов <aykononov@yandex.ru>
2020-06-15 09:44:49 +0300

    Update _XML.sql

--- _XML.sql
+++ _XML.sql
@@ -1,24 +1,23 @@
 /* Разбор XML средствами Oracle. */
 
 -- Пример создания экземпляра XMLType, с содержимым, передаваемым в строке
-SELECT XMLType(
-								'<hello-world>
-									<word seq="1">Hello</word>
-									<word seq="2">world</word>
-								</hello-world>') xml
-FROM dual;
+SELECT XMLType('<hello-world>
+                  <word seq="1">Hello</word>
+                  <word seq="2">world</word>
+                </hello-world>') xml
+  FROM dual;
 
 -- Выбираем фрагменты XML
 SELECT  t.xml.extract('//word')                AS c1
        ,t.xml.extract('//word[position()=1]')  AS c2
        ,t.xml.extract('//word[@seq=2]/text()') AS c3
   FROM (
-        SELECT XMLType(
-											'<hello-world>
-												<word seq="1">Hello</word>
-												<word seq="2">world</word>
-											</hello-world>') xml
-        FROM dual )t;
+        SELECT XMLType('<hello-world>
+                  <word seq="1">Hello</word>
+                  <word seq="2">world</word>
+                </hello-world>') xml
+          FROM dual
+        )t;
         
      C1                        C2                          C3
 ----------------------------  --------------------------  -----
