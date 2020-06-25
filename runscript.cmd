b4e19e043b84abfd2130e4eca1aa771c5a427401
Кононов Александр Юрьевич <kononov_ay@vit-spb.ru>
2020-06-25 09:50:49 +0300

    внесены изменения

--- runscript.cmd
+++ runscript.cmd
@@ -1,6 +1,6 @@
 @echo off
-cls
 chcp 1251
-echo -------------------------------- > runscript.log
-echo %DATE:~0,2%.%DATE:~3,2%.%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2% >> runscript.log
+cls
+echo --------------------------------- > runscript.log
 sqlplus SCOTT/TIGER@XE @runscript.PDC >> runscript.log
+echo %DATE:~0,2%.%DATE:~3,2%.%DATE:~6,4% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2% >> runscript.log
